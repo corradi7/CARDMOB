bc251944be8f29b4b7ede754e813a96e6fd8fef7
Livia Corradi <liviacorradi.ifsp@gmail.com>
2025-08-12 08:56:17 -0300

    Inclui ThemeContext

--- Etapa3/loja-react-app/App.tsx
+++ Etapa3/loja-react-app/App.tsx
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { ThemeProvider } from './src/contexts/ThemeContext';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
   return (
-    <HomeScreen />
+    <ThemeProvider>
+      <HomeScreen />
+    </ThemeProvider>
   );
 }
 
