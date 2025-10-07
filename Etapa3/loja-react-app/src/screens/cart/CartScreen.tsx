677f3219bcf40adbaa9cda0da91736da12c20dc8
Livia Corradi <liviacorradi.ifsp@gmail.com>
2025-10-07 08:12:43 -0300

    Carrinho de compras mínimo

--- Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
+++ Etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -1,11 +1,33 @@
 import React, { useContext } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import CartItem from './CartItem';
+
+const CartScreen = ({ navigation }: any) => {
+
+    const renderItem = ({item} : any) => (
+        <CartItem item={item} />
+    );
+
+    const cartTemp = [
+        { 
+            id: 1,
+            quantity: 2,
+            price: 8.5,
+            name: "Brigadeiro promoção especial",
+            image: "http://10.81.205.3:5000/uploads/brigadeiro-matcha-white.png",
+        }
+    ];
 
 export default CartScreen = ({ navigation }: any) => {
 
-return (
+
     <View>
         <Text> Carrinho de compras</Text>
+        <FlatList
+            data={cartTemp}
+            renderItem={renderItem}
+            keyExtractor={(item: any) => item.id.toString()}
+        />
     </View>
-);
 };
